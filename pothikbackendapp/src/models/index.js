0e8331258930664bcd50ac1628668a4cdb368319
IbrahimZihad <ibrahimzihad5@gmail.com>
2025-12-01 16:23:39 +0600

    remove index.js from models and routes folder temporarily

--- pothikbackendapp/src/models/index.js
+++ pothikbackendapp/src/models/index.js
@@ -1,52 +1,52 @@
-// src/models/index.js
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const config = require('../config');
+// // src/models/index.js
+// const fs = require('fs');
+// const path = require('path');
+// const Sequelize = require('sequelize');
+// const config = require('../config');
 
-const basename = path.basename(__filename);
-const db = {};
+// const basename = path.basename(__filename);
+// const db = {};
 
-const sequelize = new Sequelize(
-  config.db.database,
-  config.db.username,
-  config.db.password,
-  {
-    host: config.db.host,
-    port: config.db.port,
-    dialect: config.db.dialect,
-    logging: config.db.logging,
-    pool: config.db.pool,
-    define: {
-      underscored: true,
-      timestamps: true,
-    },
-  }
-);
+// const sequelize = new Sequelize(
+//   config.db.database,
+//   config.db.username,
+//   config.db.password,
+//   {
+//     host: config.db.host,
+//     port: config.db.port,
+//     dialect: config.db.dialect,
+//     logging: config.db.logging,
+//     pool: config.db.pool,
+//     define: {
+//       underscored: true,
+//       timestamps: true,
+//     },
+//   }
+// );
 
-const modelsDir = __dirname;
+// const modelsDir = __dirname;
 
-fs.readdirSync(modelsDir)
-  .filter(
-    (file) =>
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.endsWith('.model.js')
-  )
-  .forEach((file) => {
-    const model = require(path.join(modelsDir, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+// fs.readdirSync(modelsDir)
+//   .filter(
+//     (file) =>
+//       file.indexOf('.') !== 0 &&
+//       file !== basename &&
+//       file.endsWith('.model.js')
+//   )
+//   .forEach((file) => {
+//     const model = require(path.join(modelsDir, file))(sequelize, Sequelize.DataTypes);
+//     db[model.name] = model;
+//   });
 
-// Run associations
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+// // Run associations
+// Object.keys(db).forEach((modelName) => {
+//   if (db[modelName].associate) {
+//     db[modelName].associate(db);
+//   }
+// });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-db.models = db;
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+// db.models = db;
 
-module.exports = db;
+// module.exports = db;
--- pothikbackendapp/src/routes/index.js
+++ pothikbackendapp/src/routes/index.js
@@ -1,35 +1,35 @@
-const fs = require("fs");
-const path = require("path");
-const express = require("express");
+// const fs = require("fs");
+// const path = require("path");
+// const express = require("express");
 
-const router = express.Router();
+// const router = express.Router();
 
-// Path of this folder
-const routesPath = __dirname;
+// // Path of this folder
+// const routesPath = __dirname;
 
-// Read all files ending with .routes.js
-const routeFiles = fs.readdirSync(routesPath).filter((file) =>
-  file.endsWith(".routes.js")
-);
+// // Read all files ending with .routes.js
+// const routeFiles = fs.readdirSync(routesPath).filter((file) =>
+//   file.endsWith(".routes.js")
+// );
 
-routeFiles.forEach((file) => {
-  const fullPath = path.join(routesPath, file);
-  const route = require(fullPath);
+// routeFiles.forEach((file) => {
+//   const fullPath = path.join(routesPath, file);
+//   const route = require(fullPath);
 
-  // Validate export
-  if (typeof route !== "function") {
-    console.error(
-      `❌ ERROR: '${file}' does not export an Express router. Skipping this file.`
-    );
-    return;
-  }
+//   // Validate export
+//   if (typeof route !== "function") {
+//     console.error(
+//       `❌ ERROR: '${file}' does not export an Express router. Skipping this file.`
+//     );
+//     return;
+//   }
 
-  // Build route name from file → Example: users.routes.js → /users
-  const routeName = "/" + file.replace(".routes.js", "");
+//   // Build route name from file → Example: users.routes.js → /users
+//   const routeName = "/" + file.replace(".routes.js", "");
 
-  console.log(`✔ Loaded route: ${routeName} from ${file}`);
+//   console.log(`✔ Loaded route: ${routeName} from ${file}`);
 
-  router.use(routeName, route);
-});
+//   router.use(routeName, route);
+// });
 
-module.exports = router;
+// module.exports = router;
