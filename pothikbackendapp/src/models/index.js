bd116e051663174e0cd65a6c71ef3c142ab94306
Asif Mia <asifmia.dev26@gmail.com>
2025-12-01 14:51:41 +0600

    Create Relationship one to many between destination and spot

--- pothikbackendapp/src/models/index.js
+++ pothikbackendapp/src/models/index.js
@@ -1,1 +1,15 @@
 
+const Destination = require("./destination.model");
+const Spot = require("./spot.model");
+
+Destination.hasMany(Spot, {
+  foreignKey: "destination_id",
+  as: "spots",
+});
+
+Spot.belongsTo(Destination, {
+  foreignKey: "destination_id",
+  as: "destination",
+});
+
+module.exports = { Destination, Spot };
