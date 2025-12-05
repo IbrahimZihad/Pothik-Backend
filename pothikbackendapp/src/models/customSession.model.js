32e2fa02f200ae5f2c34ee132555d48e4a32f66e
Foysal khan <foysalkhan80.aa@gmail.com>
2025-12-05 06:24:43 +0600

    Added customSession.model

--- pothikbackendapp/src/models/customSession.model.js
+++ pothikbackendapp/src/models/customSession.model.js
@@ -1,1 +1,31 @@
 
+module.exports = (sequelize, DataTypes) => {
+  const CustomSession = sequelize.define(
+    "CustomSession",
+    {
+      session_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      session_data: {
+        type: DataTypes.TEXT, 
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "custom_sessions",
+      timestamps: true, 
+    }
+  );
+
+  CustomSession.associate = (models) => {
+    CustomSession.belongsTo(models.User, { foreignKey: "user_id" });
+  };
+
+  return CustomSession;
+};
