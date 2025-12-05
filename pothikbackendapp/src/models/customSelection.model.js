4c5fbee3638cfbf5dad5519e0dee6c72b9af5ca6
Foysal khan <foysalkhan80.aa@gmail.com>
2025-12-05 06:50:04 +0600

    Added customSelection.model

--- pothikbackendapp/src/models/customSelection.model.js
+++ pothikbackendapp/src/models/customSelection.model.js
@@ -1,1 +1,37 @@
 
+module.exports = (sequelize, DataTypes) => {
+  const CustomSelection = sequelize.define(
+    "CustomSelection",
+    {
+      selection_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      package_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      service_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "custom_selections",
+      timestamps: true,
+    }
+  );
+
+  CustomSelection.associate = (models) => {
+    CustomSelection.belongsTo(models.User, { foreignKey: "user_id" });
+    CustomSelection.belongsTo(models.Package, { foreignKey: "package_id" });
+    CustomSelection.belongsTo(models.PackageService, { foreignKey: "service_id" });
+  };
+
+  return CustomSelection;
+};
