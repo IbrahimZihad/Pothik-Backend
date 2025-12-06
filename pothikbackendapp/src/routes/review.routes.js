2b82f5ac0feb258e210211e7ae0545f26579a8f2
0tarun <tdas221498@bscse.uiu.ac.bd>
2025-12-07 01:03:30 +0600

    successfully added review route, controller, model

--- pothikbackendapp/src/routes/review.routes.js
+++ pothikbackendapp/src/routes/review.routes.js
@@ -1,6 +1,26 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const reviewController = require('../controllers/review.controller');
 
-// TODO: add your routes here later
+// Create review
+router.post('/', reviewController.createReview);
+
+// Get all reviews
+router.get('/', reviewController.getAllReviews);
+
+// Get review by ID
+router.get('/:id', reviewController.getReviewById);
+
+// Get reviews by user ID
+router.get('/user/:user_id', reviewController.getReviewsByUser);
+
+// Get reviews by service
+router.get('/service/:service_type/:service_id', reviewController.getReviewsByService);
+
+// Update review
+router.put('/:id', reviewController.updateReview);
+
+// Delete review
+router.delete('/:id', reviewController.deleteReview);
 
 module.exports = router;
