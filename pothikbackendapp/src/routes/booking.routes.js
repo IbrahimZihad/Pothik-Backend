cbdfca6c63ac770b9ff99dc8b4d2fbf0447ade58
IbrahimZihad <ibrahimzihad5@gmail.com>
2025-12-11 10:24:22 +0600

    updated booking routes

--- pothikbackendapp/src/routes/booking.routes.js
+++ pothikbackendapp/src/routes/booking.routes.js
@@ -3,21 +3,21 @@ const router = express.Router();
 const bookingController = require("../controllers/booking.controller");
 
 // Create booking
-router.post("/", auth, bookingController.createBooking);
+router.post("/", bookingController.createBooking);
 
 // Get all bookings
-router.get("/", auth, bookingController.getAllBookings);
+router.get("/", bookingController.getAllBookings);
 
 // Get booking by ID
-router.get("/:id", auth, bookingController.getBookingById);
+router.get("/:id", bookingController.getBookingById);
 
 // Get bookings by user
-router.get("/user/:user_id", auth, bookingController.getBookingsByUser);
+router.get("/user/:user_id", bookingController.getBookingsByUser);
 
 // Update booking status
-router.put("/:id/status", auth, bookingController.updateBookingStatus);
+router.put("/:id/status", bookingController.updateBookingStatus);
 
 // Delete booking
-router.delete("/:id", auth, bookingController.deleteBooking);
+router.delete("/:id", bookingController.deleteBooking);
 
 module.exports = router;
