380d203b1a4a0ec9cc6da8af7c7344b8eea1efc7
IbrahimZihad <ibrahimzihad5@gmail.com>
2026-01-13 23:43:27 +0600

    blog controlller and route

--- pothikbackendapp/src/routes/blog.routes.js
+++ pothikbackendapp/src/routes/blog.routes.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const blogController = require("../controllers/blog.controller.js");
 
-// TODO: add your routes here later
+// -----------------------------------------------------------------------------
+// BLOG ROUTES
+// -----------------------------------------------------------------------------
+
+// CREATE a new blog
+router.post("/blogs", blogController.createBlog);
+
+// GET all blogs
+router.get("/blogs", blogController.getAllBlogs);
+
+// GET blog by slug (must be before /:id to avoid conflict)
+router.get("/slug/:slug", blogController.getBlogBySlug);
+
+// GET blogs by user
+router.get("/user/:user_id", blogController.getBlogsByUser);
+
+// GET single blog by ID
+router.get("/:id", blogController.getBlogById);
+
+// UPDATE blog
+router.put("/:id", blogController.updateBlog);
+
+// DELETE blog
+router.delete("/:id", blogController.deleteBlog);
 
 module.exports = router;
