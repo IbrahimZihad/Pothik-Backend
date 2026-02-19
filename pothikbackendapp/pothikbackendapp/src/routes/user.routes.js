950144ec359eac5b55285baf4c7305f8cfcda778
IbrahimZihad <ibrahimzihad5@gmail.com>
2026-02-19 21:03:36 +0600

    Remove unused models, routes, and services to streamline the application structure

--- pothikbackendapp/pothikbackendapp/src/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// User routes will be added here later
-// Auth routes (register, login) are in auth.routes.js
-
-module.exports = router;
